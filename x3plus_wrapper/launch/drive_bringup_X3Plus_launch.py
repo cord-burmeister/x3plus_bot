227f2bfda5eeec61e2fb6f433414bdcafafb9478
cord-burmeister <cord.burmeister@live.de>
2025-04-19 11:05:52 +0200

    Add first version of the wrapper package

---
+++ x3plus_wrapper/launch/drive_bringup_X3Plus_launch.py
@@ -0,0 +1,187 @@
+
+from ament_index_python.packages import (
+    get_package_share_path,
+get_package_share_directory,
+)
+
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import (
+    Command,
+    PythonExpression,
+    FindExecutable,
+    PathJoinSubstitution,
+    LaunchConfiguration,
+)
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+from launch.actions import OpaqueFunction
+
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+# evaluates LaunchConfigurations in context for use with xacro.process_file(). Returns a list of launch actions to be included in launch description
+def evaluate_xacro(context, *args, **kwargs):
+
+    # namespace = LaunchConfiguration('namespace').perform(context)
+
+    # Use xacro to process the file
+    # xacro_file = os.path.join(get_package_share_directory('x3plus_description'), 'urdf', 'yahboomcar_X3plus.urdf.xacro')
+    xacro_file = os.path.join(get_package_share_directory('x3plus_description'), 'urdf', 'yahboomcar_X3plusX.urdf.xacro')
+
+    #robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = xacro.process_file(xacro_file, 
+            mappings={  
+                # "ns" : namespace
+                }).toxml()
+
+ 
+
+    robot_state_publisher_node = Node(
+      package='robot_state_publisher',
+      executable='robot_state_publisher',
+      name='robot_state_publisher',
+        output='both',
+      parameters=[{
+        'robot_description': robot_description_config
+      }])
+
+    return [robot_state_publisher_node]
+
+print("---------------------robot_type = x3plus---------------------")
+
+def generate_launch_description():
+    urdf_tutorial_path = get_package_share_path('x3plus_description')
+    default_model_path = urdf_tutorial_path / 'urdf/yahboomcar_X3.urdf'
+    wrapper_path = get_package_share_path('x3plus_wrapper')
+    default_rviz_config_path = wrapper_path / 'rviz/drive_xplus.rviz'
+
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='false', choices=['true', 'false'],
+                                    description='Flag to enable joint_state_publisher_gui')
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
+    pub_odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', default_value='false',
+                                            description='Whether to publish the tf from the original odom to the base_footprint')
+
+    # robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+    #                                    value_type=str)
+
+    # robot_state_publisher_node = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     parameters=[{'robot_description': robot_description}]
+    # )
+
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+  
+
+    driver_node = Node(
+        package='x3plus_wrapper',
+        executable='Mecanum_driver_X3Plus',
+    )
+
+#     base_node = Node(
+#         package='yahboomcar_base_node',
+#         executable='base_node_X3Plus',
+#         # 当使用ekf融合时，该tf有ekf发布
+#         parameters=[{'pub_odom_tf': LaunchConfiguration('pub_odom_tf')}]
+#     )
+
+#     imu_filter_config = os.path.join(              
+#         get_package_share_directory('yahboomcar_bringup'),
+#         'param',
+#         'imu_filter_param.yaml'
+#     ) 
+#     imu_filter_node = Node(
+#         package='imu_filter_madgwick',
+#         executable='imu_filter_madgwick_node',
+#         parameters=[imu_filter_config]
+#     )
+
+#     imu_filter_config = os.path.join(              
+#         get_package_share_directory('yahboomcar_bringup'),
+#         'param',
+#         'imu_filter_param.yaml'
+#     ) 
+
+#     ekf_node = Node(
+#          package='robot_localization',
+#          executable='ekf_node',
+#          name='ekf_filter_node',
+#          output='screen',
+#          parameters=[os.path.join(
+#              get_package_share_directory('yahboomcar_bringup'),
+#              'config/ekf.yaml')]
+# #             'config/ekf.yaml'), {'use_sim_time': use_sim_time}]
+#     )
+
+    # ekf_node = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(
+    #         get_package_share_directory('robot_localization'), 'launch'),
+    #         '/ekf_x1_x3_launch.py'])
+    # )
+
+#    yahboom_joy_node = Node(
+#        package='yahboomcar_ctrl',
+#        executable='yahboom_joy_X3',
+#    )
+
+    return LaunchDescription([
+        gui_arg,
+        model_arg,
+        rviz_arg,
+        pub_odom_tf_arg,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+#        robot_state_publisher_node,
+        driver_node,
+        OpaqueFunction(function=evaluate_xacro),        
+        rviz_node,
+        # base_node,
+        # imu_filter_node,
+        # ekf_node,
+ #       yahboom_joy_node
+    ])
---
+++ x3plus_wrapper/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+
+package_name = 'x3plus_wrapper'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),glob(os.path.join('launch','*launch.py'))),
+        (os.path.join('share', package_name,'rviz'),glob(os.path.join('rviz','*.rviz*'))),
+        (os.path.join('share', package_name, 'param'), glob(os.path.join('param', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marvin',
+    maintainer_email='cord.burmeister@live.de',
+    description='Package mapping the driver functionality into the ROS2 world',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+              'Mecanum_driver_X3Plus = x3plus_wrapper.Mecanum_driver_X3Plus:main',
+        ],
+    },
+)
